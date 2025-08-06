194544b54bdf37fdcb1dce617726dd40739f00bf
Sam A.S. Welch <116264477+sawelch-NIVA@users.noreply.github.com>
2025-08-06 18:02:38 +0200

    Remove devtools:: call for posit connect

--- app.R
+++ app.R
@@ -5,9 +5,9 @@
 # If we're not on Rstudio, install the app package from Github
 # (otherwise it searches CRAN and crashes...)
 
-if (commandArgs()[[1L]] != "RStudio") {
-  devtools::install_github("https://github.com/sawelch-NIVA/STOPeData")
-}
+# if (commandArgs()[[1L]] != "RStudio") {
+#   devtools::install_github("https://github.com/sawelch-NIVA/STOPeData")
+# }
 pkgload::load_all(export_all = FALSE, helpers = FALSE, attach_testthat = FALSE)
 options("golem.app.prod" = TRUE)
 STOPeData::run_app() # add parameters here (if any)
