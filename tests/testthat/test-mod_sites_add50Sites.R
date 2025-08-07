6dc83ea15c7db01f432599d02666e27b3b4a2ee7
Sam Welch <saw@niva.no>
2025-08-07 15:16:42 +0200

    Failed attempt to scrub dates in tests

--- tests/testthat/test-mod_sites_add50Sites.R
+++ tests/testthat/test-mod_sites_add50Sites.R
@@ -4,7 +4,7 @@ test_that("{shinytest2} add 50 blank sites to the table in mod_sites", {
   app <- AppDriver$new(name = "add50Sites")
   app$set_inputs(`main-page` = "03-sites")
 
-  for (n in 1:50) {
+  for (n in 1:5) {
     app$set_inputs(`sites-add_site` = "click")
     # give the server time to catch up
     Sys.sleep(1)
@@ -12,3 +12,17 @@ test_that("{shinytest2} add 50 blank sites to the table in mod_sites", {
 
   app$expect_values(output = "sites-sites_table")
 })
+
+# Failed to scrub dates so far
+#
+# scrub_hot_dates <- function(hot) {
+#   hot_list <- hot |> rjson::fromJSON()
+#   hot_tibble <- hot_list$x$data |> bind_rows() |> mutate(ENTERED_DATE = "")
+#   return(hot_tibble)
+# }
+#
+# print_and_return <- function(input) {
+#   output <<- input
+#   return(input)
+# }
+#
