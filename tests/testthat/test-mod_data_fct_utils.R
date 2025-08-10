e9fc5361797cdfb15b7eeec9cd91dda2239ab03f
Sam Welch <saw@niva.no>
2025-08-10 08:22:19 +0200

    Refactored llm validation and units code

--- tests/testthat/test-mod_data_fct_utils.R
+++ tests/testthat/test-mod_data_fct_utils.R
@@ -1,3 +1,23 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+describe("parameter_units() loads expected data", {
+  withr::local_dir(new = "../") # set wd to root
+  it(description = "loads a dataframe with the right four columns", {
+    units <- parameter_units()
+    testthat::expect_equal(
+      object = names(units),
+      expected = c(
+        "MEASURED_UNIT",
+        "BASE_SI_UNIT",
+        "CONVERSION_FACTOR",
+        "UNIT_COMMENTS"
+      )
+    )
+  })
+  it(description = "selected by named column", {
+    units_selected <- parameter_units("BASE_SI_UNIT")
+
+    testthat::expect_vector(
+      object = units_selected,
+      ptype = character()
+    )
+  })
 })
