d1c0d1f1ee36f48b5d818f1b6b4baf4c41c68027
Sam A.S. Welch <116264477+sawelch-NIVA@users.noreply.github.com>
2025-10-20 15:29:15 +0200

    Removed a call to devtools::

--- app.R
+++ app.R
@@ -5,8 +5,6 @@
 # If we're not on Rstudio, install the app package from Github
 # (otherwise it searches CRAN and crashes...)
 
-devtools::install_github("https://github.com/sawelch-NIVA/STOPeData")
-
 pkgload::load_all(export_all = FALSE, helpers = FALSE, attach_testthat = FALSE)
 options(golem.app.prod = TRUE)
 options(bslib.color_contrast_warnings = FALSE)
